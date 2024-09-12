dc38c6f563da4390f06d2b5db82664bddc53102c
shakil <shakil.dgency@gmail.com>
2024-09-12 13:02:03 +0600

    test webhooks

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -822,6 +822,7 @@ export interface ApiCountryCountry extends Schema.CollectionType {
       'api::country-scholarship.country-scholarship'
     >;
     scholarship: Attribute.Component<'vstudy.multiple-section'>;
+    test: Attribute.String;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
